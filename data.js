0ba2ae8ce1225cbcbbc69112fd944902d367f905
Rajan77770 <twinklepasswan@gmail.com>
2026-02-21 14:47:47 +0530

    i

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
-/* HireSphere GOD DATA ENGINE */
-
 function getCandidates(){
 
 try{
 
 return JSON.parse(
+
 localStorage.getItem("candidates")
+
 )||[];
 
 }catch{
@@ -16,26 +16,33 @@ return[];
 
 }
 
+
 function saveCandidates(data){
 
 localStorage.setItem(
+
 "candidates",
+
 JSON.stringify(data)
+
 );
 
 }
 
-/* SAFE NAME FIX */
 
 function getName(c){
 
-return(
+if(!c) return "Unknown";
+
+return (
 
 c.name ||
+
 c.fullname ||
-c.candidateName ||
-(c.email ? c.email.split("@")[0] : "") ||
-"Candidate"
+
+c.candidate ||
+
+"Unknown"
 
 );
 
