d1421e5c815d66e0afd2a53f7c560049c3fbcd33
Rajan77770 <twinklepasswan@gmail.com>
2026-02-22 02:20:59 +0530

    data

--- data.js
+++ data.js
@@ -1,7 +1,84 @@
-let data = JSON.parse(localStorage.getItem("data")) || [];
+// ============================
+// HIRESPHERE MASTER DATA SYSTEM
+// ============================
 
-function saveData(){
+let hiresphere = JSON.parse(
+localStorage.getItem("hiresphere")
+) || {
 
-localStorage.setItem("data",JSON.stringify(data));
+jobs:[],
+applications:[],
+candidates:[]
+
+};
+
+
+// SAVE MASTER DATA
+
+function saveDB(){
+
+localStorage.setItem(
+
+"hiresphere",
+
+JSON.stringify(hiresphere)
+
+);
+
+}
+
+
+
+// ============================
+// JOB FUNCTIONS
+// ============================
+
+function addJob(job){
+
+hiresphere.jobs.push(job);
+
+saveDB();
+
+}
+
+
+function getJobs(){
+
+return hiresphere.jobs;
+
+}
+
+
+
+// ============================
+// APPLY FUNCTIONS
+// ============================
+
+function applyJob(app){
+
+hiresphere.applications.push(app);
+
+saveDB();
+
+}
+
+
+function getApplications(){
+
+return hiresphere.applications;
+
+}
+
+
+
+// ============================
+// HR STATUS UPDATE
+// ============================
+
+function updateStatus(index,status){
+
+hiresphere.applications[index].status = status;
+
+saveDB();
 
 }
